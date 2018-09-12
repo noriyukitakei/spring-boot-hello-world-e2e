aa13cb1bcce8a8274247cd62a02a65955e79686c
TAKEI Noriyuki <ntakei@sios.com>
2018-09-13 00:16:46 +0900

    modified

---
+++ src/test/java/com/sios/test/E2ETest.java
@@ -0,0 +1,34 @@
+package com.sios.test;
+import static org.junit.Assert.*;
+
+import org.junit.Test;
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.chrome.ChromeOptions;
+
+public class E2ETest {
+	
+	@Test
+	public void test() throws InterruptedException {
+		// ChromeDriverまでのパスを設定する
+		System.setProperty("webdriver.chrome.driver", "/var/lib/jenkins/chromedriver");
+
+		ChromeOptions chromeOptions = new ChromeOptions();
+		chromeOptions.addArguments("--headless");
+		// Chromeドライバーインスタンスを作成する
+		WebDriver driver = new ChromeDriver(chromeOptions);
+
+		driver.get("https://ntakeidevops.azurewebsites.net/helloworld/hello");
+
+		WebElement searchElement = driver.findElement(By.id("hello"));
+		String title = searchElement.getText();
+		
+		// ブラウザーを閉じる
+		driver.quit();
+		
+		assertEquals(title, "Hello World!!aa");
+	}
+
+}

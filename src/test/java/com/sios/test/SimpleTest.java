7ebc242f40bc6f13db51fb27ace5331d88ed45bf
TAKEI Noriyuki <ntakei@sios.com>
2018-09-12 17:16:45 +0900

    first commit

---
+++ src/test/java/com/sios/test/SimpleTest.java
@@ -0,0 +1,54 @@
+package com.sios.test;
+import static org.junit.Assert.*;
+
+import org.junit.Test;
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.support.ui.ExpectedCondition;
+import org.openqa.selenium.support.ui.WebDriverWait;
+
+public class SimpleTest {
+
+	@Test
+	public void test() throws InterruptedException {
+		// ChromeDriverまでのパスを設定する
+		System.setProperty("webdriver.chrome.driver", "/Users/ntakei/data/tmp/20180910/chromedriver");
+
+		// Chromeドライバーインスタンスを作成する
+		WebDriver driver = new ChromeDriver();
+
+		// Google.comにアクセスする
+		driver.get("https://www.google.com");
+
+		// 表示しているページのTitleを出力する
+		System.out.println("Page title is: " + driver.getTitle());
+
+		String title = driver.getTitle();
+		Thread.sleep(1000);
+
+		// 検索ワード入力エレメントに、検索ワードを渡して、submitする
+		WebElement searchElement = driver.findElement(By.name("q"));
+		searchElement.sendKeys("Cheese!");
+
+		Thread.sleep(1000);
+
+		searchElement.submit();
+
+		// ページが更新するまで待つ（Timeoutは10秒)
+		new WebDriverWait(driver, 10).until(
+				(ExpectedCondition<Boolean>) webDriver -> webDriver.getTitle().toLowerCase().startsWith("cheese!"));
+
+		// ページ更新後のTitleを出力する
+		System.out.println("Page title is: " + driver.getTitle());
+
+		Thread.sleep(1000);
+
+		// ブラウザーを閉じる
+		driver.quit();
+		
+		assertEquals(title, "Google!!!!");
+	}
+
+}

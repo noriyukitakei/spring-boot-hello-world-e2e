2d2842421196dd9e073ad5fa34348a588614c2b4
TAKEI Noriyuki <ntakei@sios.com>
2018-09-12 21:40:16 +0900

    modified

--- src/test/java/com/sios/test/SimpleTest.java
+++ src/test/java/com/sios/test/SimpleTest.java
@@ -23,36 +23,15 @@ public class SimpleTest {
 		// Chromeドライバーインスタンスを作成する
 		WebDriver driver = new ChromeDriver(chromeOptions);
 
-		// Google.comにアクセスする
-		driver.get("https://www.google.com");
+		driver.get("https://ntakeidevops.azurewebsites.net/helloworld/hello");
 
-		// 表示しているページのTitleを出力する
-		System.out.println("Page title is: " + driver.getTitle());
-
-		String title = driver.getTitle();
-		Thread.sleep(1000);
-
-		// 検索ワード入力エレメントに、検索ワードを渡して、submitする
-		WebElement searchElement = driver.findElement(By.name("q"));
-		searchElement.sendKeys("Cheese!");
-
-		Thread.sleep(1000);
-
-		searchElement.submit();
-
-		// ページが更新するまで待つ（Timeoutは10秒)
-		new WebDriverWait(driver, 10).until(
-				(ExpectedCondition<Boolean>) webDriver -> webDriver.getTitle().toLowerCase().startsWith("cheese!"));
-
-		// ページ更新後のTitleを出力する
-		System.out.println("Page title is: " + driver.getTitle());
-
-		Thread.sleep(1000);
-
+		WebElement searchElement = driver.findElement(By.id("hello"));
+		String title = searchElement.getText();
+		
 		// ブラウザーを閉じる
 		driver.quit();
 		
-		assertEquals(title, "Google");
+		assertEquals(title, "Hello World!!");
 	}
 
 }

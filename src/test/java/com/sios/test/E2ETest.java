1c822714add8aa6d5e010b60b9dc66ae4b5b744e
TAKEI Noriyuki <ntakei@sios.com>
2018-09-23 08:10:38 +0900

    固有の設定を環境変数から取得するようにした

--- src/test/java/com/sios/test/E2ETest.java
+++ src/test/java/com/sios/test/E2ETest.java
@@ -12,22 +12,36 @@ public class E2ETest {
 	
 	@Test
 	public void test() throws InterruptedException {
+		
+		// テストを実施したいアプリケーションのURLを環境変数から取得する。
+		String url = System.getenv("HELLOWORLD_URL");
+		
+		// ChromeDriverまでのパスを環境変数から取得する。
+		String webdriverPath = System.getenv("WEBDRIVER_PATH");
+		
 		// ChromeDriverまでのパスを設定する
-		System.setProperty("webdriver.chrome.driver", "/var/lib/jenkins/chromedriver");
+		System.setProperty("webdriver.chrome.driver", webdriverPath);
 
+		// Chromeをheadlessモードで起動する。
 		ChromeOptions chromeOptions = new ChromeOptions();
 		chromeOptions.addArguments("--headless");
-		// Chromeドライバーインスタンスを作成する
+		
+		// Chromeドライバーインスタンスを作成する。
 		WebDriver driver = new ChromeDriver(chromeOptions);
 
-		driver.get("https://ntakeidevops.azurewebsites.net/helloworld/hello");
-
+		// テストを実施したいアプリケーションのURLを設定する。
+		driver.get(url);
+		
+		// idがhelloという要素を取得する。
+		// 実際には<h1 id='hello'>Hello World!!</h1>というHTMLの
+		// Hello World!!という部分を取得する処理である。
 		WebElement searchElement = driver.findElement(By.id("hello"));
 		String title = searchElement.getText();
 		
 		// ブラウザーを閉じる
 		driver.quit();
 		
+		// 画面に表示されている文字列がHello World!!かどうかをチェックする。
 		assertEquals(title, "Hello World!!");
 	}
 
